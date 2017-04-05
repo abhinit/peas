3638272f8c5d115220a8b9ed529a50e7b0447158
He Buwei <hebuweitom@gmail.com>
2017-04-05 16:53:59 +0800

    change taskflow

--- taskflow/app/app.py
+++ taskflow/app/app.py
@@ -11,9 +11,11 @@ from taskflow import task
 
 class FileReadTask(task.Task):
 
+
     default_provides = 'file_read_result'
 
     def execute(self):
+        print("in the file read")
         with open('small.data') as f:
             dataframe = pd.read_csv(f)
             return dataframe.to_json()
@@ -24,7 +26,7 @@ class StandardizationTask(task.Task):
     default_provides = 'standardization_result'
 
     def execute(self, standardization_input):
-
+        print("in the standardisation task")
         req = urllib2.Request('http://192.168.99.100:30583')
         req.add_header('Content-Type', 'application/json')
         req_data = '\"data\": \"' + standardization_input + '\"'
@@ -34,6 +36,7 @@ class StandardizationTask(task.Task):
 
 class ClassificationTask(task.Task):
     def execute(self, classification_input):
+        print("in classification task")
         print("Executing '%s'" % (self.name))
         print("Got input '%s'" % (a))
 
@@ -43,6 +46,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def run_app():
+
+    print("got request")
 
     wf = linear_flow.Flow("flow")
     wf.add(FileReadTask(), StandardizationTask(rebind={'standardization_input': 'file_read_result'}))
